e584d8a69156259d3f125c8231d1158c3fa327aa
Benito J. Gonzalez <bgonzalez@unicon.net>
2016-07-15 11:44:31 -0700

    Add Ant to download-tools.sh

--- download-tools.sh
+++ download-tools.sh
@@ -2,6 +2,7 @@
 #
 # Download Ant, Maven, Tomcat and JDK for uPortal -- current as of 7/15/2016
 #
+wget http://archive.apache.org/dist/ant/binaries/apache-ant-1.8.2-bin.tar.gz
 wget http://archive.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
 wget http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.35/bin/apache-tomcat-8.0.35.tar.gz
