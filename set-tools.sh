f437299f73625657881f393900fd89800f64b2cc
Benito J. Gonzalez <bgonzalez@unicon.net>
2016-07-15 11:09:21 -0700

    Fixing path issue in set-tools.sh

--- set-tools.sh
+++ set-tools.sh
@@ -2,7 +2,7 @@
 #
 # Set up tools in local directory
 
-tar xzf src/apache-tomcat-8.0.35.tar.gz && ln -s apache-tomcat-8.0.35 tomcat
-tar xzf src/apache-ant-1.8.2-bin.tar.gz && ln -s apache-ant-1.8.2 ant
-tar xzf src/apache-maven-3.3.9-bin.tar.gz && ln -s apache-maven-3.3.9 maven
-tar xzf src/jdk-8u92-linux-x64.tar.gz && ln -s jdk1.8.0_92 java
+tar xzf apache-tomcat-8.0.35.tar.gz && ln -s apache-tomcat-8.0.35 tomcat
+tar xzf apache-ant-1.8.2-bin.tar.gz && ln -s apache-ant-1.8.2 ant
+tar xzf apache-maven-3.3.9-bin.tar.gz && ln -s apache-maven-3.3.9 maven
+tar xzf jdk-8u92-linux-x64.tar.gz && ln -s jdk1.8.0_92 java
