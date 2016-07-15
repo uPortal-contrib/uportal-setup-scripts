d991c30ff6afa7e2f84554b3ecc5e2731d18a467
Benito J. Gonzalez <bgonzalez@unicon.net>
2016-07-15 11:13:57 -0700

    Updating setenv.sh for Java 8 prod use

--- setenv.sh
+++ setenv.sh
@@ -1,1 +1,3 @@
-JAVA_OPTS="$JAVA_OPTS -XX:+PrintCommandLineFlags -XX:MaxPermSize=512m -Xms1G -Xmx1G -Dorg.jasig.resourceserver.utils.aggr.aggregated_theme=false"
+export JAVA_MIN_HEAP=2g
+export JAVA_MAX_HEAP=4g
+export CATALINA_OPTS="-server -d64 -XX:+PrintCommandLineFlags -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:logs/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=logs/heapdump-`date +%Y-%m-%d.%H:%M:%S`.hprof -Xms${JAVA_MIN_HEAP} -Xmx${JAVA_MAX_HEAP} -XX:MaxMetaspaceSize=512m -XX:+UseG1GC -XX:MaxGCPauseMillis=200"
